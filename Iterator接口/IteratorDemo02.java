9c15a2ff28bd3751a03ae8c0118fdf3dec3a8e3e
Iyamaya <44797261+Iyamaya@users.noreply.github.com>
2020-07-22 22:37:05 -0500

    Add files via upload

--- Iterator接口/IteratorDemo02.java
+++ Iterator接口/IteratorDemo02.java
@@ -1,7 +1,7 @@
 import java.util.List ;
 import java.util.ArrayList ;
 import java.util.Iterator ;
-public class IteratorDemo01{
+public class IteratorDemo02{
 	public static void main(String args[]){
 		List<String> all=  new ArrayList<String>() ;	// 
 		all.add("hello") ;
@@ -9,7 +9,13 @@ public class IteratorDemo01{
 		all.add("world") ;
 		Iterator<String> iter = all.iterator() ;	// 为Iterator接口实例化
 		while(iter.hasNext()){	// 判断是否有内容
-			System.out.println(iter.next()) ;	// 输出内容
+			String str = iter.next() ;
+			if("_".equals(str)){
+				iter.remove() ;	// 删除元素		
+			}else{
+				System.out.println(str) ;	// 输出内容
+			}
 		}
+		System.out.println("删除之后的集合：" + all) ;
 	}
 };
