ba91dbc0cf3af1128e630ebfac8251fdb47685f7
Iyamaya <44797261+Iyamaya@users.noreply.github.com>
2020-07-22 22:23:56 -0500

    Add files via upload

--- Set接口/TreeSetDemo01.java
+++ Set接口/TreeSetDemo01.java
@@ -1,14 +1,14 @@
-import java.util.HashSet ;
+import java.util.TreeSet ;
 import java.util.Set ;
-public class HashSetDemo01{
+public class TreeSetDemo01{
 	public static void main(String args[]){
-		Set<String> allSet = new HashSet<String>() ;
-		allSet.add("A") ;	// 增加内容
-		allSet.add("B") ;	// 增加内容
+		Set<String> allSet = new TreeSet<String>() ;
 		allSet.add("C") ;	// 增加内容
 		allSet.add("C") ;	// 重复内容
 		allSet.add("C") ;	// 重复内容
 		allSet.add("D") ;	// 增加内容
+		allSet.add("B") ;	// 增加内容
+		allSet.add("A") ;	// 增加内容
 		allSet.add("E") ;	// 增加内容
 		System.out.println(allSet) ;
 	}
