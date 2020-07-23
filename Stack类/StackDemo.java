cb2c183c4b4106fae73262b0499b28d19b48954a
Iyamaya <44797261+Iyamaya@users.noreply.github.com>
2020-07-23 00:18:01 -0500

    Add files via upload

--- Stack类/StackDemo.java
+++ Stack类/StackDemo.java
@@ -1,1 +1,13 @@
-
+import java.util.Stack ;
+public class StackDemo{
+	public static void main(String args[]){
+		Stack<String> s = new Stack<String>() ;
+		s.push("A") ;	// »Î’ª
+		s.push("B") ;	// »Î’ª
+		s.push("C") ;	// »Î’ª
+		System.out.print(s.pop() + "°¢") ;
+		System.out.print(s.pop() + "°¢") ;
+		System.out.println(s.pop() + "°¢") ;
+		System.out.println(s.pop()) ;
+	}
+};
